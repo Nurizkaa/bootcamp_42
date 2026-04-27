495c0620a00d3e7739d33c4e71b338ed75ca26ec
nazbiyke08 <akyshovanazbiyke@gmail.com>
2026-04-27 17:27:42 +0600

    крч

--- my_bot.py
+++ my_bot.py
@@ -5,4 +5,12 @@ bot = telebot.TeleBot('8754534207:AAEC4oRIlgN6FRDSW-pOQvap-VQkWW0G6Gs')
 def start(message):
     bot.send_message(message.chat.id, 'Привет дорогуша')
 
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id,)
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    bot.send_message(message.chat.id, message.text)
+
 bot.polling(none_stop=True)
